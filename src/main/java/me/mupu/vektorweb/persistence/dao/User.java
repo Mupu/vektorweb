38454cbb8482bcb5c239244a8367047ba7815b50
Mupu <Mupuk55@web.de>
2019-02-28 15:37:26 +0100

    hibernate example

--- src/main/java/me/mupu/vektorweb/persistence/dao/User.java
+++ src/main/java/me/mupu/vektorweb/persistence/dao/User.java
@@ -10,7 +10,7 @@ import javax.persistence.*;
 public class User extends BaseEntitiy {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "userID")
     private long id;
 
--- src/main/java/me/mupu/vektorweb/presentation/controller/PublicController.java
+++ src/main/java/me/mupu/vektorweb/presentation/controller/PublicController.java
@@ -1,16 +1,27 @@
 package me.mupu.vektorweb.presentation.controller;
 
 import me.mupu.vektorweb.CustomUser;
+import org.hibernate.Session;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.persistence.EntityManager;
 import java.util.Base64;
 
 
 @Controller
 public class PublicController {
+
+    @Autowired
+    private EntityManager entityManager;
+
+
+    private Session getSession() {
+        return entityManager.unwrap(Session.class);
+    }
 
     @RequestMapping("/login")
     public ModelAndView login(Authentication authentication) {
@@ -29,6 +40,17 @@ public class PublicController {
         mv.setViewName("everyone/home");
 
         mv.addObject("b64", Base64.getEncoder());
+
+
+//        Session s = getSession();
+//        var u = new User();
+//        u.setUsername("test");
+//        u.setEmail("test");
+//        u.setConfirmationToken("test");
+//        u.setResetPasswordToken("test");
+//        s.persist(u);
+//        getSession().beginTransaction().commit();
+
 
         if (authentication != null)
             mv.addObject("user", ((CustomUser) authentication.getPrincipal()).getUser());
